ffb29ff008bbc5cd4535dc8be52d91ab2c9616eb
Bilgehan Korkmaz <bilgehan1981@gmail.com>
2015-08-04 16:53:10 +0300

    Multi Object Life Management
    Manage multiple object life in Unity

---
+++ MultiObjectManagement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class GenericGameItem
+{
+    public GameObject GameItem;
+    public Stack ItemList;
+    public int PreloadListSize;
+    public bool Resizable;
+}
+
+public class MultiObjectManagement : MonoBehaviour
+{
+    public static MultiObjectManagement current;
+    public List<GenericGameItem> GameItemList = new List<GenericGameItem>();
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    void Start()
+    {
+        for (int k = 0; k < GameItemList.Count; k++)
+        {
+            GameItemList[k].ItemList = new Stack();
+
+            for (int m = 0;m < GameItemList[k].PreloadListSize;m++)
+            {
+                GameObject clone = Instantiate(GameItemList[k].GameItem) as GameObject;
+                clone.SetActive(false);
+                GameItemList[k].ItemList.Push(clone);
+            }
+        }
+    }
+
+    public void SpawnGameItem(int index)
+    {
+        if (GameItemList[index].ItemList.Count > 0)
+        {
+            GameObject ListItem = GameItemList[index].ItemList.Pop() as GameObject;
+            ListItem.SetActive(true);
+            ListItem.transform.position = GameItemList[index].GameItem.transform.position;
+        }
+
+        else if (GameItemList[index].Resizable == true)
+        {
+            GameObject InstantiatedClone = Instantiate(GameItemList[index].GameItem) as GameObject;
+            InstantiatedClone.SetActive(true);
+            InstantiatedClone.transform.position = GameItemList[index].GameItem.transform.position;
+        }
+    }
+
+    public void DeactivateGameItem(GameObject deactivated, int index)
+    {
+        deactivated.SetActive(false);
+        GameItemList[index].ItemList.Push(deactivated);
+    }
+}
