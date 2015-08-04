77e5bebe01c74f65f4f6629ebca1e0d020c5224d
Bilgehan Korkmaz <bilgehan1981@gmail.com>
2015-08-04 16:58:39 +0300

    Move object

---
+++ ObjectMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMove : MonoBehaviour
+{
+    public float speed = 15;
+
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
